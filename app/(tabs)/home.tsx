5fc93b1b9ea5c11ab2d656780dc23585d4ede24d
Paun Wander <tsme0613@gmail.com>
2025-03-01 02:06:25 +0800

    Replaced fonts to use in ThemedText

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -5,17 +5,22 @@ import { Colors } from "@/constants/Colors"
 import { name } from "@/constants/Config"
 import React from "react"
 
-const debug_border = false
+const debug_border = true
 
 export default function HomeScreen() {
   return (
     <View style={[styles.container, debug_border && styles.debug_border]}>
       <View style={[styles.welcomeContainer, debug_border && styles.debug_border]}>
-        <ThemedText type={"title"}>Welcome {name}</ThemedText>
+        <ThemedText type={"title"} style={[styles.textBox, debug_border && styles.debug_border]}>
+          Tracker
+        </ThemedText>
+        <ThemedText type={"default"} style={[styles.textBox, { fontSize: 24 }, debug_border && styles.debug_border]}>
+          {name}
+        </ThemedText>
       </View>
       <View style={[styles.mainContainer, debug_border && styles.debug_border]}>
         <ThemedText style={{ color: Colors.light.text }} type={"default"}>
-          Welcome!
+          Text
         </ThemedText>
       </View>
     </View>
@@ -29,12 +34,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     marginTop: 20,
     paddingTop: 10,
-    // backgroundColor: "white",
+    backgroundColor: Colors.dark.background,
   },
   welcomeContainer: {
     flex: 1,
+    flexDirection: "row",
     width: "90%",
-    alignItems: "flex-start",
+    alignItems: "stretch",
     justifyContent: "center",
     margin: 2,
   },
@@ -43,9 +49,10 @@ const styles = StyleSheet.create({
     width: "100%",
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "white",
     borderTopStartRadius: 30,
     borderTopEndRadius: 30,
+    backgroundColor: Colors.light.background,
   },
+  textBox: { flex: 1, paddingTop: 5 },
   debug_border: { borderWidth: 2, borderColor: "rgb(255, 0, 0)" },
 })
